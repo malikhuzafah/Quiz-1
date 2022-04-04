6e7a474450ce862d4cd073cb57860c384ea1b4f2
Malik Huzafah Khan <huzafahrajput10@gmail.com>
2022-04-05 01:19:50 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).on("click", 'a[href^="#"]', function (event) {
     event.preventDefault();
     $("html, body").animate({
-        scrollTop: $($.attr(this, "href")).offset().top,
+            scrollTop: $($.attr(this, "href")).offset().top - 40,
         },
         500
     );
@@ -11,3 +11,22 @@ function opennav() {
     var nav = document.getElementById("navbarSupportedContent");
     nav.style.height = "0";
 }
+
+
+$(document).ready(function(){
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        if(value == "all") {
+            $('.filter').show('1000');
+        }
+        else {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+        }
+    });
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+    console.log($('#navbar').height());
+});
